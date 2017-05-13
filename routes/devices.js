83da456e4deea3070ad7fabc0802f2a00406f4a4
Alex Wacker <alex@alexwacker.com>
2017-05-13 11:18:15 -0400

    update device location on post from the scanners

--- routes/devices.js
+++ routes/devices.js
@@ -2,6 +2,8 @@ var express = require('express');
 var list = require('../firebase/list');
 
 var router = express.Router();
+var firebase = require('firebase');
+var db = firebase.database();
 
 /* GET manage-devices page. */
 router.get('/manage', function(req, res, next) {
@@ -27,6 +29,24 @@ router.get('/details/:id', function(req, res, next) {
     console.log(req.param('id'));
     user = req.params.id;
     res.render('devices-details', { title: 'Manage Device Groups', user: user });
+});
+
+router.post('/:deviceid/updatelocation', function(req, res, next) {
+    var deviceid = req.params.deviceid;
+    var roomid = req.body.roomid;
+    var buildingid = req.body.buildingid;
+
+    db.ref('/devices/'+deviceid).once('value').then(function(snapshot) {
+        var device = snapshot.val();
+
+        device.room = roomid;
+        device.building = buildingid;
+
+        db.ref('/devices/'+deviceid).set(device).then(function(){
+            res.statusCode(204);
+        });
+    });
+
 });
 
 module.exports = router;
