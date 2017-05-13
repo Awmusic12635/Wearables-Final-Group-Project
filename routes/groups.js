558435bd083a3a496e17315d33c91048c8cfe8f2
jrgabler <jrg4017@rit.edu>
2017-05-13 11:18:34 -0400

    refactor groups and rfid, add firebase functions, fix update

---
+++ routes/groups.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var list = require('../firebase/list');
+
+router.get('/manage', function(req, res, next) {
+    var data = {};
+
+    Promise.all([list('/groups')])
+        .then(function (snapshots) {
+            data.groups = snapshots[0];
+            // data.userCount = (snapshots[0].users).length;
+
+            data.title = 'Manage User Groups';
+            res.render('groups-manage', data);
+        });
+});
+
+router.get('/groups', function (req, res, next) {
+   res.render('groups-users', { title: 'Manage User Groups'})
+});
+
+router.get('/details/:id', function(req, res, next) {
+    console.log(req.params.id);
+    res.render('users-details', { title: 'User Detail' });
+});
+
+module.exports = router;
